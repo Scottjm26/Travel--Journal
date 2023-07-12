1a4c17dfb6254adccf68400912afed37b648f090
scott <scottjm@nsuok.edu>
2023-07-12 12:09:11 -0500

    src

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,8 @@ import React from "react"
 export default function Header(){
 
 
-    return (<div>
-        <h2 className ="header">my travel journal</h2>
+    return (<div className ="header">
+        <div ><img className ="globe"src="./earth.png"/></div>
+        <h2 > my travel journal.</h2>
         </div>)
 }
