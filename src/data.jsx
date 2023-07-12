d4942ac1c2c0458e76bca472ca71c5d82d9783b2
scott <scottjm@nsuok.edu>
2023-07-12 14:26:32 -0500

    src/data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -9,7 +9,7 @@ export default [
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     },
     {
-        title: "Sydney Opera House",
+        title: "Sydne Opera House",
         location: "AUSTRALIA",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "27 May, 2021",
@@ -24,6 +24,6 @@ export default [
         startDate: "01 Oct, 2021",
         endDate: "18 Nov, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "./fjord.jpg"
+        imageUrl: "fjord.jpg"
     }
 ]
